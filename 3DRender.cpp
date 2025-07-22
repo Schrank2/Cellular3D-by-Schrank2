378af1117ebc61091165ba6eb221580138e135f6
Schrank <schrank1311@gmail.com>
2025-07-22 09:45:09 +0200

    Made the triangles render

--- 3DRender.cpp
+++ 3DRender.cpp
@@ -29,8 +29,8 @@ void readVoxels(const std::vector<std::vector<int>>& GameMap) {
 	for (int i = 0; i < GameWidth; i++) {
 		for (int j = 0; j < GameHeight; j++) {
 			if (GameMap[i][j] == 1) {
-				SDL_FColor color{ 0.0f, 255.0f, 0.0f, 255.0f };
-				POS3D pos(i, j, 3);
+				SDL_FColor color{0.0f, 0.5f, 0.0f, 1.0f};
+				POS3D pos(i, j, 1);
 				Voxel v=Voxel(pos, color);
 				VoxelQueue.emplace_back(v);
 			}
@@ -38,24 +38,29 @@ void readVoxels(const std::vector<std::vector<int>>& GameMap) {
 	}
 }
 float ScreenCoordinateX(float x, float z) {
-	return ScreenWidth * x / z + ScreenWidth / 2;
+	float scale = ScreenWidth / static_cast<float>(GameWidth);
+	return (x / z) * scale;
 }
 float ScreenCoordinateY(float y, float z) {
-	return -ScreenHeight * y / z + ScreenHeight / 2;
+	float scale = ScreenHeight / static_cast<float>(GameHeight);
+	return (y / z) * scale;
 }
 void renderVoxel(Voxel V) {
-	cout << "Rendering Voxel at (" << V.position.x << ", " << V.position.y << ", " << V.position.z << ") with color ("
-		<< V.color.r << ", " << V.color.g << ", " << V.color.b << ", " << V.color.a << ")" << endl;
+	//cout << "Rendering Voxel at (" << V.position.x << ", " << V.position.y << ", " << V.position.z << ") with color (" << V.color.r << ", " << V.color.g << ", " << V.color.b << ", " << V.color.a << ")" << endl;
 	vector<SDL_Vertex> vertices(3);
+	
 	SDL_FPoint A = {ScreenCoordinateX(V.position.x, V.position.z), ScreenCoordinateY(V.position.y, V.position.z)};
-	SDL_FPoint B = {ScreenCoordinateX(V.position.x, V.position.z), ScreenCoordinateY(V.position.y, V.position.z)};
-	SDL_FPoint C = {ScreenCoordinateX(V.position.x, V.position.z), ScreenCoordinateY(V.position.y, V.position.z)};
+	SDL_FPoint B = {ScreenCoordinateX((V.position.x+1), V.position.z), ScreenCoordinateY(V.position.y, V.position.z)};
+	SDL_FPoint C = {ScreenCoordinateX((V.position.x+1), V.position.z), ScreenCoordinateY((V.position.y+1), V.position.z)};
 	vertices[0].position = A;
 	vertices[1].position = B;
 	vertices[2].position = C;
 	vertices[0].color = V.color;
 	vertices[1].color = V.color;
 	vertices[2].color = V.color;
+	vertices[0].tex_coord = { 0.0f, 0.0f };
+	vertices[1].tex_coord = { 0.0f, 0.0f };
+	vertices[2].tex_coord = { 0.0f, 0.0f };
 	SDL_RenderGeometry(renderer, nullptr, vertices.data(), vertices.size(), nullptr, 0); // Clear previous geometry
 };
 void render3D() {
