db6c796b1c4cd03dcf508dc17ebea834e6bed0c6
Schrank <schrank1311@gmail.com>
2025-10-06 08:55:46 +0200

    centered the image

--- 3DRender.cpp
+++ 3DRender.cpp
@@ -68,14 +68,14 @@ inline static void readVoxels(const std::vector<std::vector<std::vector<int>>>& 
 	if (Debug == true) {ReadVoxelTime = SDL_GetTicks() - ReadVoxelTime; }
 }
 inline static float ScreenCoordinateX(float x, float z) {
-	float Depth = 1+z+CameraZ; // Adjusting depth for perspective
-	float scale = ScreenWidth / static_cast<float>(GameWidth);
-	return ((x+CameraX) / Depth) * scale;
+	float Depth = z+CameraZ; // Adjusting depth for perspective
+	//float scale = ScreenWidth / static_cast<float>(GameWidth);
+	return ((x+CameraX) / Depth) + (ScreenWidth/2);
 }
 inline static float ScreenCoordinateY(float y, float z) {
 	float Depth = 1+z+CameraZ; // Adjusting depth for perspective
-	float scale = ScreenHeight / static_cast<float>(GameHeight);
-	return ((y+CameraY) / Depth) * scale;
+	//float scale = ScreenHeight / static_cast<float>(GameHeight);
+	return ((y+CameraY) / Depth) + (ScreenHeight / 2);
 }
 inline static void renderVoxel(Voxel V) {
 	//cout << "Rendering Voxel at (" << V.position.x << ", " << V.position.y << ", " << V.position.z << ") with color (" << V.color.r << ", " << V.color.g << ", " << V.color.b << ", " << V.color.a << ")" << endl;
