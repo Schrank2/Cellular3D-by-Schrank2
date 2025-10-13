e4a76fa98e5ad1d7a00470198e5c692c6dc5423b
Schrank <schrank1311@gmail.com>
2025-10-13 08:38:27 +0200

    thread pooling kind of works (very unusual implementation)

--- loop.cpp
+++ loop.cpp
@@ -103,7 +103,7 @@ int game() {
 			for (int i = 0; i < ThreadCountUsed; i++) {
 				int yMin = i * rowLength;
 				int yMax = (i == ThreadCountUsed - 1) ? GameHeight : (i + 1) * rowLength; // the last thread takes the remaining rows
-				ThreadPool[i] = { CellularAutomataRules, 0, GameWidth, yMin, yMax };
+				ThreadPool[i] = thread(CellularAutomataRules, 0, GameWidth, yMin, yMax);
 			}
 			for (auto& th : ThreadPool) { th.join(); };
 			// Apply Changes
